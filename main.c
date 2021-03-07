91714a85b5e83625c3162617b3bb6fd2dfe579d3
Casey Levitt <clevitt1@jh.edu>
2021-03-07 15:15:51 -0500

    outlined trace reading

--- main.c
+++ main.c
@@ -10,11 +10,22 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
+#include <stdint.h>
 
 int main(int argc, char **argv) {
-  printf("%d\n", argc);
-  for (int i = 0; i < argc; i++) {
-    printf("%s", argv[i]);
-  }
+  assert(argc == 7); // 7 total - executable and 6 args
+  // can set these using argv and strtoi
+  uint32_t sets, blocks, blockSize, writeAllocate, writeThrough, lru;
+  // can read strings from the trace file using scanf!
+  char str[20];
+  scanf("%s", str);     // can definitely put these in a loop
+  printf("%s\n", str);   // i was experimenting
+  scanf("%s", str);
+  printf("%s\n", str);
+  scanf("%s", str);
+  printf("%s\n", str);
+  scanf("%s", str);
+  printf("%s\n", str);
   return 0;
 }
