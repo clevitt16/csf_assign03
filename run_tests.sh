fbfe4e80cb36ffab7e95fec3cbb4d25136c2c2f6
Trisha Karani <trisha.karani@gmail.com>
2021-03-22 23:17:01 -0400

    fixed shell script

--- run_tests.sh
+++ run_tests.sh
@@ -31,6 +31,30 @@ echo "no-write-allocate write-through lru" >> Results_$1_$2_$3.txt
 ./csim $1 $2 $3 no-write-allocate write-through lru < gcc.trace >> Results_$1_$2_$3.txt
 
 #swim.trace commands
+echo "" >> Results_$1_$2_$3.txt
+echo "swim.trace" > Results_$1_$2_$3.txt
+echo "" >> Results_$1_$2_$3.txt
+echo "write-allocate write-back fifo" >> Results_$1_$2_$3.txt
+./csim $1 $2 $3 write-allocate write-back fifo < swim.trace >> Results_$1_$2_$3.txt
+
+echo "write-allocate write-through fifo" >> Results_$1_$2_$3.txt
+./csim $1 $2 $3 write-allocate write-through fifo < swim.trace >> Results_$1_$2_$3.txt
+
+echo "" >> Results_$1_$2_$3.txt
+echo "no-write-allocate write-through fifo" >> Results_$1_$2_$3.txt
+./csim $1 $2 $3 no-write-allocate write-through fifo < swim.trace >> Results_$1_$2_$3.txt
+
+echo "" >> Results_$1_$2_$3.txt
+echo "write-allocate write-back lru" >> Results_$1_$2_$3.txt
+./csim $1 $2 $3 write-allocate write-back lru < swim.trace >> Results_$1_$2_$3.txt
+
+echo "" >> Results_$1_$2_$3.txt
+echo "write-allocate write-through lru" >> Results_$1_$2_$3.txt
+./csim $1 $2 $3 write-allocate write-through lru < swim.trace >> Results_$1_$2_$3.txt
+
+echo "" >> Results_$1_$2_$3.txt
+echo "no-write-allocate write-through lru" >> Results_$1_$2_$3.txt
+./csim $1 $2 $3 no-write-allocate write-through lru < swim.trace >> Results_$1_$2_$3.txt
 echo "SWIM.TRACE" >> Results_$1_$2_$3.txt
 echo "" >> Results_$1_$2_$3.txt
 echo "write-allocate write-back fifo" >> Results_$1_$2_$3.txt
